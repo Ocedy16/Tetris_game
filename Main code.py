5a732993f08629fc22a8a53b01e1fa9daf7de8dc
Delphine-RF <119135925+Delphine-RF@users.noreply.github.com>
2023-01-05 11:19:22 +0100

    Create Main code.py

--- Main code.py
+++ Main code.py
@@ -2,7 +2,6 @@ import pygame
 import random
 from pygame.locals import *  # importer les constantes
 from pygame import mixer
-import copy
 
 # Autre façon de voir les formes. A tester. Mettre toutes les coordonées des zéros dans des listes à l'intérieur d'une liste
 T = [[[2, 1], [2, 2], [1, 2], [3, 2]], [[2, 1], [2, 2], [2, 3], [3, 2]], [[1, 2], [2, 2], [3, 2], [2, 3]],
@@ -25,16 +24,15 @@ J = [[[3, 1], [1, 2], [2, 2], [3, 2]], [[1, 1], [2, 1], [2, 2], [2, 3]], [[1, 2]
 formes = [T, S, L, Z, I, O, J]
 
 class Piece(object):
-    def __init__(self, lettre, couleur, colonne, ligne):
+    def __init__(self, lettre, colonne, ligne):
         self.x = colonne
         self.y = ligne
-        #self.couleur = (random.randint(10, 254), random.randint(10, 254), random.randint(10, 254))
-        self.couleur= couleur
+        self.couleur = (random.randint(10, 254), random.randint(10, 254), random.randint(10, 254))
         self.lettre = lettre
         self.rotation = 0
         self.forme = self.lettre[self.rotation]
         # Voir pour peut être ajouter un sous accès liste si on accède d'abord aux formes.
-        #print(f"Piece Constructor / Forme : {self.forme} / X: {self.x} / Y: {self.y}")
+        print(f"Piece Constructor / Forme : {self.forme} / X: {self.x} / Y: {self.y}")
         self.bloc_1 = [self.x + self.forme[0][0], self.y + self.forme[0][1]]
         self.bloc_2 = [self.x + self.forme[1][0], self.y + self.forme[1][1]]
         self.bloc_3 = [self.x + self.forme[2][0], self.y + self.forme[2][1]]
@@ -49,11 +47,11 @@ class Piece(object):
         self.blocs = [self.bloc_1, self.bloc_2, self.bloc_3, self.bloc_4]
 
     def tourner(self):
-        #print("----------------------")
-        #print(f"SELF Forme : {self.forme}")
-        #print(f"SELF Lettre : {self.lettre}")
-        #print(f"SELF Rotation : {self.rotation}")
-        #print(f"Forme : {self.forme}")
+        print("----------------------")
+        print(f"SELF Forme : {self.forme}")
+        print(f"SELF Lettre : {self.lettre}")
+        print(f"SELF Rotation : {self.rotation}")
+        print(f"Forme : {self.forme}")
         if self.rotation <= len(self.lettre):
             self.forme = self.lettre[self.rotation]
             self.bloc_1 = [self.x + self.forme[0][0], self.y + self.forme[0][1]]
@@ -67,12 +65,12 @@ class Piece(object):
 # par contre il y a peut-être des erreurs ou oublis quand j'ai défini les propriété d'un bloc/tetros
 
 # les couleurs qui correspondent aux blocs dans le jeu classique
-couleur_forme = [[0, 255, 255], [255, 255, 0], [255, 0, 0], [0, 255, 0], [255, 0, 255], [255, 100, 10],
-                 [0, 0, 100]]  # cyan, jaune, rouge, vert, violet, orange, navy
+couleur_forme = [[0, 191, 255], [238, 201, 0], [255, 0, 0], [0, 238, 0], [171, 130, 255], [255, 128, 0], [139, 76, 57]]  # bleu, jaune, rouge, vert, violet, orange, marron
 
 dico_forme_couleur_tuple = {formes[1]:(255,0,255), formes[2]:(0,255,0), formes[3]:(255,0,0), formes[4]:(0,0,100), formes[5]:(255,255,0), formes[6]:(0,255,255), formes[7]:(255,100,10)}
 
 def creer_grille(grille_finie):
+    if type(grille_finie) == list: print('creer_grille errer')
     grille = [[(0, 0, 0) for x in range(10)] for y in
               range(20)]  # Reinitialisation de la grille. Le tuple indique la couleur
 
@@ -83,7 +81,6 @@ def creer_grille(grille_finie):
                 c = grille_finie[(j, i)]
                 grille[i][j] = c
     return grille
-
 
 def convertir_orientation_piece(piece):
     l = len(piece.lettre)
@@ -111,21 +108,15 @@ def espace_valide(piece, grille_finie):
 def verifier_defaite(grille_finie, grille):
     i = 0
     while i < len(grille[0]):
-        if (0,i) in grille_finie:
+        if grille_finie[(0, i)] != (0, 0, 0):
             return True
         i = i + 1
     return False
 
-def verifier_defaite_debut(piece,grille_finie):
-     if espace_valide(piece, grille_finie)==False:
-          return False
-     return True
-
 
 def get_shape():
     global formes, couleur_forme
-    new_int=random.randint(0,7)
-    return Piece(formes[new_int],couleur_forme[new_int], 3, 0)
+    return Piece(random.choice(formes), 3, 0)
 
 
 def dessiner_piece(new_piece):
@@ -136,26 +127,23 @@ def dessiner_piece(new_piece):
     pygame.display.update()
     pygame.init()
 
-
-def dessiner_piece_suivante(piece_suivante):
-    pygame.draw.rect(fenetre, (255, 255, 255), (20 * 15, 0 * 15, 13 * 6, 13 * 6))
+def dessiner_piece_suivante (piece_suivante):
+    pygame.draw.rect(fenetre, (255,255,255), (20*15,0*15,13*6,13*6))
     new_piece = copy.deepcopy(piece_suivante)
     new_piece.x = 20
     new_piece.deplacer()
-    # print (new_piece.x)
+    #print (new_piece.x)
     new_piece.y = 0
     new_piece.deplacer()
-    pygame.draw.rect(fenetre, new_piece.couleur, (new_piece.bloc_1[0] * 15, new_piece.bloc_1[1] * 15, 13, 13))
-    pygame.draw.rect(fenetre, new_piece.couleur, (new_piece.bloc_2[0] * 15, new_piece.bloc_2[1] * 15, 13, 13))
-    pygame.draw.rect(fenetre, new_piece.couleur, (new_piece.bloc_3[0] * 15, new_piece.bloc_3[1] * 15, 13, 13))
-    pygame.draw.rect(fenetre, new_piece.couleur, (new_piece.bloc_4[0] * 15, new_piece.bloc_4[1] * 15, 13, 13))
+    pygame.draw.rect(fenetre, new_piece.couleur,(new_piece.bloc_1[0]*15, new_piece.bloc_1[1]*15, 13, 13))
+    pygame.draw.rect(fenetre, new_piece.couleur, (new_piece.bloc_2[0]*15, new_piece.bloc_2[1]*15, 13, 13))
+    pygame.draw.rect(fenetre, new_piece.couleur, (new_piece.bloc_3[0]*15, new_piece.bloc_3[1]*15, 13, 13))
+    pygame.draw.rect(fenetre, new_piece.couleur, (new_piece.bloc_4[0]*15, new_piece.bloc_4[1]*15, 13, 13))
     pygame.display.update()
     pygame.init()
-
-
+     
 def ecrire_texte_milieu(texte, taille, couleur, surface):
     return 0
-
 
 def dessiner_grille(grille):
     # Pour chaque élément dans la grille, dessiner sa couleur
@@ -168,34 +156,38 @@ def dessiner_grille(grille):
     pygame.init()
 
 
-def retirer_lignes_pleine(grille,grille_finie):
-    inc = 0
-    for i in range(len(grille) - 1, -1, -1):
-        row = grille[i]
-        if (0, 0, 0) not in row:
-            inc += 1
-            ind = i
-            for j in range(len(row)):
-                if grille_finie.get((j, i)):
-                    del grille_finie[(j, i)]
-    if inc > 0:
-        for key in sorted(list(grille_finie), key=lambda x: x[1])[::-1]:
-            x, y = key
-            if y < ind:
-                newKey = (x, y + inc)
-                grille_finie[newKey] = grille_finie.pop(key)
+def retirer_lignes_pleine(grille_finie):
+     nb_lignes=0
+     lignes_effacees=[]
+    for j in range (20):
+      count=0
+      for i in range (10):
+              if (i,j) in grille_finie:
+                  count=count+1
+      if count==10:
+          nb_lignes=nb_lignes+1
+          lignes_effacees.append(j)
+          for k in range (10):
+                 del grille_finie[(k,j)]
+    if nb_lignes !=0:
+     for l in range (nb_lignes):
+       for j in range (lignes_effacees[l]):
+          for i in range(10):
+               if (9-i,19-j) in grille_finie:
+                    couleur=grille_finie[(9-i,19-j)]
+                    grille_finie[(9-i,(19-j)+1)]=couleur
+                    
 
-
-# def afficher_score():
-# ecrire le code
+#def afficher_score():
+    #ecrire le code
 
 def main_screen():
     begin = True
     pygame.display.set_caption('Tetris')
-    font = pygame.font.SysFont('inkfree', 30, italic=True, bold=True)  
+    font = pygame.font.SysFont('inkfree', 30, italic=True, bold=True)  # try inkfree, georgia,impact,dubai,arial
 
     text = font.render('Press any key to play', True,
-                       (255, 255, 255))  
+                       (255, 255, 255))  # This creates a new Surface with the specified text rendered on it
     textrect = text.get_rect()
     textrect.center = (600 // 2, 700 // 2)
 
@@ -209,33 +201,6 @@ def main_screen():
             if event.type == pygame.KEYDOWN:
                 begin = False
                 main()
-
-            pygame.display.update()
-          
-def end_screen():
-    begin = True
-    pygame.display.set_caption('Tetris')
-    font = pygame.font.SysFont('inkfree', 30, italic=True, bold=True)  # try inkfree, georgia,impact,dubai,arial
-
-    text = font.render('You loose : press return key to play again, or press space key to end', True,
-                       (255, 255, 255)) 
-    textrect = text.get_rect()
-    textrect.center = (600 // 2, 700 // 2)
-
-    while begin:
-        fenetre.blit(text, textrect)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.display.quit()
-                quit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE :
-                    pygame.display.quit()
-                    quit()
-                if event.key == pygame.K_RETURN:
-                    begin = False
-                    main()
 
             pygame.display.update()
 
@@ -253,12 +218,12 @@ def main():
     dessiner_piece_suivante(piece_suivante)
     time_elapsed_since_last_action = 0
 
+
     while running:
         dt = clock.tick()
         grille = creer_grille(grille_finie)
         time_elapsed_since_last_action += dt
-        vitesse=1
-        if time_elapsed_since_last_action+vitesse > 1000:
+        if time_elapsed_since_last_action > 1000:
             time_elapsed_since_last_action = 0
             piece.y += 1
             piece.deplacer()
@@ -268,9 +233,6 @@ def main():
                 save_dict(piece, grille_finie)
                 piece = copy.deepcopy(piece_suivante)
                 piece_suivante = get_shape()
-                if verifier_defaite_debut(piece,grille_finie)==False:
-                    running=False
-                    end_screen()
 
             else:
                 grille = creer_grille(grille_finie)
@@ -278,7 +240,7 @@ def main():
                 piece.deplacer()
                 dessiner_piece(piece)
                 dessiner_piece_suivante(piece_suivante)
-
+                    
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -317,10 +279,6 @@ def main():
                         save_dict(piece, grille_finie)
                         piece = copy.deepcopy(piece_suivante)
                         piece_suivante = get_shape()
-                        if verifier_defaite_debut(piece,grille_finie)==False:
-                              running=False
-                              end_screen()
-                        retirer_lignes_pleine(grille, grille_finie)
 
                     else:
                         grille = creer_grille(grille_finie)
@@ -328,11 +286,10 @@ def main():
                         piece.deplacer()
                         dessiner_piece(piece)
                         dessiner_piece_suivante(piece_suivante)
-
-
+                    
                 if event.key == pygame.K_SPACE:
                     convertir_orientation_piece(piece)
-                    # piece.tourner(piece.lettre)
+                    #piece.tourner(piece.lettre)
                     piece.tourner()
                     if not (espace_valide(piece, grille_finie)):
                         piece.rotation = (piece.rotation - 1) % len(piece.forme)
@@ -342,19 +299,15 @@ def main():
                         dessiner_grille(grille)
                         piece.tourner()
                         dessiner_piece(piece)
-                        dessiner_piece_suivante(piece_suivante)
-        retirer_lignes_pleine(grille, grille_finie)
-        if verifier_defaite(grille_finie,grille):
-            running=False
-            end_screen()
-        # afficher_score()
- 
+                        dessiner_piece_suivante(piece_suivante)            
+        retirer_lignes_pleine(grille_finie)
+        #afficher_score()
+
+          
 
 pygame.init()
 fenetre = pygame.display.set_mode((600, 700))
-"""
 mixer.init()
-mixer.music.load('D:/Dossier Océane/Tetris_music.mp3.mp3')
+mixer.music.load('D:/Dossier Océane/Tetris_music.mp3')
 mixer.music.play()
-"""
 main_screen()
