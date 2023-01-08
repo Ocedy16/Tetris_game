902d4aaf17b72f90c96e1b99e5b8ed52259e2111
Ocedy16 <118558735+Ocedy16@users.noreply.github.com>
2023-01-08 10:07:07 +0100

    Update Main code.py

--- Main code.py
+++ Main code.py
@@ -9,86 +9,9 @@ T = [[[2, 1], [2, 2], [1, 2], [3, 2]], [[2, 1], [2, 2], [2, 3], [3, 2]], [[1, 2]
      [[2, 1], [2, 2], [2, 3], [1, 2]]]
 L = [[[1, 1], [1, 2], [2, 2], [3, 2]], [[2, 1], [2, 2], [1, 3], [2, 3]], [[1, 2], [2, 2], [3, 2], [3, 3]],
      [[2, 1], [3, 1], [2, 2], [2, 3]]]
-S = [[[2, 2], [3, 2], [3, 3], [4, 3]], [[2, 1], [1, 2], [2, 2], [1, 3]]]
-Z = [[[2, 2], [3, 2], [1, 3], [2, 3]]]
-I = [[[2, 0], [2, 1], [2, 2], [2, 3]], [[0, 2], [1, 2], [2, 2], [3, 2]]]
-O = [[[2, 2], [3, 2], [2, 3], [3, 3]]]
-J = [[[3, 1], [1, 2], [2, 2], [3, 2]], [[1, 1], [2, 1], [2, 2], [2, 3]], [[1, 2], [2, 2], [3, 2], [1, 3]],
-     [[2, 1], [2, 2], [2, 3], [3, 3]]]
-
-# Ainsi de suite avec les autres. Puis lier cela aux coordonnées de la pièce. Ex: le x et le y de la pièce représentent tout en haut à gauche de la pièce et
-# les x et y de combien on doit "bouger" pour tomber sur le bloc. Il faut donc considérer les blocs indépendamment. Peut être implémenter qqc dans la classe.
-# Une chose à tester pour voir si la méthode est la bonne: implémenter une classe dans un autre fichier avec les 4 coordonnées du bloc. Voir si modifier
-# Le x et le y du bloc change bien les sous coordonnées automatiquement. En gros, est ce que le calcul se fait automatiquement. Je pense que oui mais j'aimerais
-# Être sûre.
-
-formes = [T, S, L, Z, I, O, J]
-
-
-class Piece(object):
-    def __init__(self, lettre, colonne, ligne):
-        self.x = colonne
-        self.y = ligne
-        self.couleur = (random.randint(10, 254), random.randint(10, 254), random.randint(10, 254))
-        self.lettre = lettre
-        self.rotation = 0
-        self.forme = self.lettre[self.rotation]
-        # Voir pour peut être ajouter un sous accès liste si on accède d'abord aux formes.
-        print(f"Piece Constructor / Forme : {self.forme} / X: {self.x} / Y: {self.y}")
-        self.bloc_1 = [self.x + self.forme[0][0], self.y + self.forme[0][1]]
-        self.bloc_2 = [self.x + self.forme[1][0], self.y + self.forme[1][1]]
-        self.bloc_3 = [self.x + self.forme[2][0], self.y + self.forme[2][1]]
-        self.bloc_4 = [self.x + self.forme[3][0], self.y + self.forme[3][1]]
-        self.blocs = [self.bloc_1, self.bloc_2, self.bloc_3, self.bloc_4]
-
-    def deplacer(self):
-        self.bloc_1 = [self.x + self.forme[0][0], self.y + self.forme[0][1]]
-        self.bloc_2 = [self.x + self.forme[1][0], self.y + self.forme[1][1]]
-        self.bloc_3 = [self.x + self.forme[2][0], self.y + self.forme[2][1]]
-        self.bloc_4 = [self.x + self.forme[3][0], self.y + self.forme[3][1]]
-        self.blocs = [self.bloc_1, self.bloc_2, self.bloc_3, self.bloc_4]
-
-    def tourner(self):
-        print("----------------------")
-        print(f"SELF Forme : {self.forme}")
-        print(f"SELF Lettre : {self.lettre}")
-        print(f"SELF Rotation : {self.rotation}")
-        print(f"Forme : {self.forme}")
-        if self.rotation <= len(self.lettre):
-            self.forme = self.lettre[self.rotation]
-            self.bloc_1 = [self.x + self.forme[0][0], self.y + self.forme[0][1]]
-            self.bloc_2 = [self.x + self.forme[1][0], self.y + self.forme[1][1]]
-            self.bloc_3 = [self.x + self.forme[2][0], self.y + self.forme[2][1]]
-            self.bloc_4 = [self.x + self.forme[3][0], self.y + self.forme[3][1]]
-            self.blocs = [self.bloc_1, self.bloc_2, self.bloc_3, self.bloc_4]
-
-
-# ce que j'ai codé pour définir les blocs, c'est toujours la même idée mais je trouve ça plus clair que les tableaux S,Z,... au début
-# par contre il y a peut-être des erreurs ou oublis quand j'ai défini les propriété d'un bloc/tetros
-
-# les couleurs qui correspondent aux blocs dans le jeu classique
-couleur_forme = [[0, 255, 255], [255, 255, 0], [255, 0, 0], [0, 255, 0], [255, 0, 255], [255, 100, 10],
-                 [0, 0, 100]]  # cyan, jaune, rouge, vert, violet, orange, navy
-"""
-dico_forme_couleur_tuple = {formes[0]: (255, 0, 255), formes[1]: (0, 255, 0), formes[2]: (255, 0, 0),
-                            formes[3]: (0, 0, 100), formes[4]: (255, 255, 0), formes[5]: (0, 255, 255),
-                            formes[6]: (255, 100, 10)}
-"""
-
-import pygame
-import random
-from pygame.locals import *  # importer les constantes
-from pygame import mixer
-import copy
-
-# Autre façon de voir les formes. A tester. Mettre toutes les coordonées des zéros dans des listes à l'intérieur d'une liste
-T = [[[2, 1], [2, 2], [1, 2], [3, 2]], [[2, 1], [2, 2], [2, 3], [3, 2]], [[1, 2], [2, 2], [3, 2], [2, 3]],
-     [[2, 1], [2, 2], [2, 3], [1, 2]]]
-L = [[[1, 1], [1, 2], [2, 2], [3, 2]], [[2, 1], [2, 2], [1, 3], [2, 3]], [[1, 2], [2, 2], [3, 2], [3, 3]],
-     [[2, 1], [3, 1], [2, 2], [2, 3]]]
-S = [[[2, 2], [3, 2], [3, 3], [4, 3]], [[2, 1], [1, 2], [2, 2], [1, 3]]]
-Z = [[[2, 2], [3, 2], [1, 3], [2, 3]], [[2, 3], [2, 2], [3, 3], [3, 4]]]
-I = [[[2, 1], [2, 2], [2, 3], [2, 4]], [[0, 3], [1, 3], [2, 3], [3, 3]]]
+S = [[[2, 1], [3, 1], [2, 2], [1, 2]], [[2, 2], [3, 2], [2, 1], [3, 3]]]
+Z = [[[2, 1], [3, 2], [1, 1], [2, 2]], [[2, 2], [3, 1], [3, 2], [2, 3]]]
+I = [[[2, 1], [2, 4], [2, 2], [2, 3]], [[0, 3], [1, 3], [2, 3], [3, 3]]]
 O = [[[2, 2], [3, 2], [2, 3], [3, 3]]]
 J = [[[3, 1], [1, 2], [2, 2], [3, 2]], [[1, 1], [2, 1], [2, 2], [2, 3]], [[1, 2], [2, 2], [3, 2], [1, 3]], [[2, 1], [2, 2], [2, 3], [3, 3]]]
 
@@ -125,19 +48,15 @@ class Piece(object):
         self.bloc_4 = [self.x + self.forme[3][0], self.y + self.forme[3][1]]
         self.blocs = [self.bloc_1, self.bloc_2, self.bloc_3, self.bloc_4]
 
-    def tourner(self):
-        #print("----------------------")
-        #print(f"SELF Forme : {self.forme}")
-        #print(f"SELF Lettre : {self.lettre}")
-        #print(f"SELF Rotation : {self.rotation}")
-        #print(f"Forme : {self.forme}")
-        if self.rotation <= len(self.lettre):
-            self.forme = self.lettre[self.rotation]
-            self.bloc_1 = [self.x + self.forme[0][0], self.y + self.forme[0][1]]
-            self.bloc_2 = [self.x + self.forme[1][0], self.y + self.forme[1][1]]
-            self.bloc_3 = [self.x + self.forme[2][0], self.y + self.forme[2][1]]
-            self.bloc_4 = [self.x + self.forme[3][0], self.y + self.forme[3][1]]
-            self.blocs = [self.bloc_1, self.bloc_2, self.bloc_3, self.bloc_4]
+    def tourner(self,grille_finie):
+        self.rotation=(self.rotation+1)%len(self.lettre)
+        self.forme=self.lettre[self.rotation]
+        self.deplacer()
+        if not espace_valide(self,grille_finie):
+            self.rotation= (self.rotation-1)%len(self.lettre)
+            self.forme=self.lettre[self.rotation]
+            self.deplacer()
+
 
 
 # ce que j'ai codé pour définir les blocs, c'est toujours la même idée mais je trouve ça plus clair que les tableaux S,Z,... au début
@@ -161,9 +80,6 @@ def creer_grille(grille_finie):# liste des blocs avec tuple = couleur et indice 
     return grille
 
 
-def convertir_orientation_piece(piece):
-    l = len(piece.lettre)
-    piece.rotation = (piece.rotation + 1) % l
 
 
 def save_dict(piece, grille_finie):
@@ -176,7 +92,7 @@ def espace_valide(piece, grille_finie):
     for bloc in piece.blocs:
         if bloc[0] < 0 or bloc[0] > 9:
             return False
-        if 0 > bloc[1] < 0 or bloc[1] > 19:
+        if bloc[1] < 0 or bloc[1] > 19:
             return False
         if (bloc[0], bloc[1]) in grille_finie:
             return False
@@ -445,18 +361,11 @@ def main():
 
 
                 if event.key == pygame.K_SPACE:
-                    convertir_orientation_piece(piece)
-                    # piece.tourner(piece.lettre)
-                    piece.tourner()
-                    if not (espace_valide(piece, grille_finie)):
-                        piece.rotation = (piece.rotation - 1) % len(piece.forme)
-                        piece.tourner()
-                    else:
-                        grille = creer_grille(grille_finie)
-                        dessiner_grille(grille)
-                        piece.tourner()
-                        dessiner_piece(piece)
-                        dessiner_piece_suivante(piece_suivante)
+                    piece.tourner(grille_finie)
+                    grille = creer_grille(grille_finie)
+                    dessiner_grille(grille)
+                    dessiner_piece(piece)
+                    dessiner_piece_suivante(piece_suivante)
         #retirer_lignes_pleine(grille, grille_finie, nb_lignes_total)
         change_duree(grille,grille_finie)
         #if verifier_defaite(grille_finie,grille):
